462997d0afea6ae398d058ca3e999f1f1827a4f0
hadidelbari-ux <hadidelbari99@gmail.com>
2026-06-15 16:47:27 +0330

    Update main.py
    3

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-
+import os
 import requests
 
-TOKEN = "8957547593:AAGUNoxn0aku5Pv5ukzqUiDr6WTQmXdgf2w"
-
+TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = "@material_daily_price"
 
 def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, data=data)
 
-url = f"https://api.telegram.org/bot8957547593:AAGUNoxn0aku5Pv5ukzqUiDr6WTQmXdgf2w/sendMessage"
-
-data = {
-
-"chat_id": CHAT_ID,
-
-"تست اولیه": text
-import os
-
-print("TOKEN:", os.environ.get("BOT_TOKEN"))
-}
+send_message("سیستم پایش قیمت آهن فعال شد")
