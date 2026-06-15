1d3fb326059ca052e85716ef9dac4dd8f82faf66
hadidelbari-ux <hadidelbari99@gmail.com>
2026-06-15 14:10:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+import requests
+
+TOKEN = "8957547593:AAGUNoxn0aku5Pv5ukzqUiDr6WTQmXdgf2w"
+
+CHAT_ID = "@material_daily_price"
+
+def send_message(text):
+
+url = f"https://api.telegram.org/bot8957547593:AAGUNoxn0aku5Pv5ukzqUiDr6WTQmXdgf2w/sendMessage"
+
+data = {
+
+"chat_id": CHAT_ID,
+
+"تست اولیه": text
+
+}
